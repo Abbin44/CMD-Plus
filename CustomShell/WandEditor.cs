3375935d14206373a19a75479c7f6c689a932cd8
Abbin44 <abbin44@gmail.com>
2021-03-03 01:38:20 +0100

    Added syntax highlight

--- CustomShell/WandEditor.cs
+++ CustomShell/WandEditor.cs
@@ -53,12 +53,14 @@ namespace CustomShell
             main.wandTextBox.Clear();
             main.wandTextBox.Visible = true;// Swap text box to be able to 
             main.outputBox.Visible = false; // preseve coloring in previous commands
-            main.wandTextBox.ForeColor = Color.Aqua;
+            main.wandTextBox.ForeColor = Color.LightSteelBlue;
             try
             {
                 string[] lines = File.ReadAllLines(path);
                 for (int i = 0; i < lines.Length; i++)
                     AddTextToConsole(lines[i]);
+
+                ApplySyntaxHighlight();
 
                 main.wandTextBox.Focus();
                 main.wandTextBox.SelectionStart = main.wandTextBox.TextLength;
@@ -71,6 +73,64 @@ namespace CustomShell
                 main.outputBox.Visible = true;
                 main.AddTextToConsole("Something went wrong, please check your input");
                 return;
+            }
+        }
+
+        public void ApplySyntaxHighlight()
+        {
+            string[] types = new string[] {"int ", "integer ", "float ", "single ", "double ", "decimal ", "bool ", "boolean ", "string "};
+            string[] operators = new string[] {"!", "=", ">", "<", "|"};
+            string[] statements = new string[] {"if", "else if", "elif", "if else", "else", "true", "false", "try", "catch", "finally", "public", "private", "protected", "static", "using", "import", "void", "while", "for", "return", "continue", "break"};
+
+            string word;
+
+
+            for (int i = 0; i < types.Length; i++)
+            {
+                int s_start = main.wandTextBox.SelectionStart, startIndex = 0, index;
+                word = types[i];
+                while ((index = main.wandTextBox.Text.IndexOf(word, startIndex)) != -1)
+                {
+                    main.wandTextBox.Select(index, word.Length);
+                    main.wandTextBox.SelectionColor = Color.Blue;
+
+                    startIndex = index + word.Length;
+                }
+
+                main.wandTextBox.SelectionStart = s_start;
+                main.wandTextBox.SelectionLength = 0;
+            }
+
+            for (int i = 0; i < operators.Length; i++)
+            {
+                int s_start = main.wandTextBox.SelectionStart, startIndex = 0, index;
+                word = operators[i];
+                while ((index = main.wandTextBox.Text.IndexOf(word, startIndex)) != -1)
+                {
+                    main.wandTextBox.Select(index, word.Length);
+                    main.wandTextBox.SelectionColor = Color.Red;
+
+                    startIndex = index + word.Length;
+                }
+
+                main.wandTextBox.SelectionStart = s_start;
+                main.wandTextBox.SelectionLength = 0;
+            }
+
+            for (int i = 0; i < statements.Length; i++)
+            {
+                int s_start = main.wandTextBox.SelectionStart, startIndex = 0, index;
+                word = statements[i];
+                while ((index = main.wandTextBox.Text.IndexOf(word, startIndex)) != -1)
+                {
+                    main.wandTextBox.Select(index, word.Length);
+                    main.wandTextBox.SelectionColor = Color.Purple;
+
+                    startIndex = index + word.Length;
+                }
+
+                main.wandTextBox.SelectionStart = s_start;
+                main.wandTextBox.SelectionLength = 0;
             }
         }
 
